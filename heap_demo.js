a68edc924eb0874a4c268a1a57f1ee83b6bff9d4
hchiam <hchiam@users.noreply.github.com>
2021-04-09 23:36:45 -0400

    heap_demo.js from my older repo

---
+++ heap_demo.js
@@ -0,0 +1,16 @@
+// npm install
+// node heap_demo.js
+
+require("google-closure-library"); // npm install -D google-closure-library
+
+goog.require("goog.structs.Heap");
+
+const heap = new goog.structs.Heap();
+heap.insert("some key", "some value");
+console.log(heap.peek());
+
+// things like heap:
+// https://github.com/google/closure-library/tree/master/closure/goog/structs
+
+// and demos like advanced tooltip:
+// https://google.github.io/closure-library/source/closure/goog/demos/
